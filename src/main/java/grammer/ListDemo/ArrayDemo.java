1d2e7a37b2399059197f0359ef636add2f6280f2
GrandFisher <470911438@qq.com>
2019-04-08 18:19:45 +0800

    题目

--- src/main/java/grammer/ListDemo/ArrayDemo.java
+++ src/main/java/grammer/ListDemo/ArrayDemo.java
@@ -40,5 +40,6 @@ public class ArrayDemo {
             System.out.println(a.get(i));
         }
 
+
     }
 }
--- src/main/java/grammer/ListDemo/QueueDemo.java
+++ src/main/java/grammer/ListDemo/QueueDemo.java
@@ -6,5 +6,8 @@ import java.util.Queue;
 public class QueueDemo {
     public static void main(String[] args) {
         Queue<Integer> q=new LinkedList<>();
+        q.offer(1);
+        q.peek();
+        q.poll();
     }
 }
